eac69c45f29204869d4864cd31deedcf23f1b943
BHTISKI <selimtabak805@gmail.com>
2026-08-28 15:28:03 +0300

    Update test setup: move cleanup to individual test files

--- src/testUtils/setup.ts
+++ src/testUtils/setup.ts
@@ -1,9 +1,2 @@
 import '@testing-library/jest-dom/vitest';
-import { afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-import { resetMockRandom } from './mockRandom';
-
-afterEach(() => {
-  cleanup();
-  resetMockRandom();
-});
+// Global setup for tests
